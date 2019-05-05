87bc19276fb2d6fbe5c0fdb2169a3d91b38dbed7
Dementr-Slash <dmi56756877@yandex.ru>
2019-05-05 22:08:16 +0900

    for add

---
+++ for.php
@@ -0,0 +1,72 @@
+<?php
+$arr = ["Ivanov", "Petrov", "Sidorov"];
+$arr[] = "Smirnov"; 
+for ($i = 0; $i < count($arr); $i++) {
+    echo $arr[$i] . "<br/>";
+}
+$names = [
+  'Ivan' => "Ivanov",  
+  'Petr' => "Petrov",  
+  'Sidor' => "Sidorov",  
+];
+foreach ($names as $name => $value) {
+    echo $name . " " .$value . "<br>";
+}
+?>
+
+<!--Таблица умножения-->
+<?php 
+$rows = 10;
+$cols = 10;
+echo '<table border="1">';
+    for ($tr = 1; $tr <= $rows; $tr++){
+        echo"<tr>";
+
+        for ($td = 1; $td <= $cols; $td++){
+            echo"<td style ='width : 20px'>" . ($td * $tr) . '</td>';
+        }
+        echo '</tr>';
+    }
+echo '</table>';
+echo '<br>';
+
+//Таблица умножения 2
+
+$rows = 10; // количество строк, tr
+$cols = 10; // количество столбцов, td
+
+$table = '<table border="1">';
+
+for ($tr=1; $tr<=$rows; $tr++){
+    $table .= '<tr>';
+    for ($td=1; $td<=$cols; $td++){
+        $table .= '<td>'. $tr*$td .'</td>';
+    }
+    $table .= '</tr>';
+}
+
+$table .= '</table>';
+echo $table; // сделали эхо всего 1 раз
+echo '<br>';
+?>
+
+<!--Таблица умножения 3-->
+<?php 
+$rows = 10; // количество строк, tr
+$cols = 10; // количество столбцов, td
+?>
+<table border="1">
+<?php for ($tr=1; $tr<=$rows; $tr++) { ?>
+    <tr>
+    <?php for ($td = 1; $td <=$cols; $td++){ ?>
+        <td> <?= $td*$tr ?> </td>
+    <? } ?>
+    </tr >
+<? } ?>
+</table>
+<?= "<br>"?>
+<select>
+    <?php for ($year = 1900; $year <= 2019; $year++) { ?>
+    <option><?= $year?></option>
+    <? } ?>
+</select>
--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 //require_once 'if.php';
 //require_once 'switch.php';
 //require_once 'while.php';
-require_once 'rulet.php';
+//require_once 'rulet.php';
 //require_once 'home1/write.php';
 //require_once 'home2/writeDate.php';
 //require_once 'home3/Write_extention.php';
 //require_once 'array.php';
 //require_once "func.php";
+require_once "for.php";
 
 
